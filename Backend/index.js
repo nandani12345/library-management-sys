dcae260df961f5b8810554f029289c7595c556eb
Nandani Kumari <165877222+nandani12345@users.noreply.github.com>
2025-02-06 19:02:33 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,11 +12,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.listen(9000, async () => {
+let port = process.env.PORT || 5000;
+
+app.listen(port, async () => {
   try {
     await connection;
     console.log("Connect to database");
-    console.log("Server has started at http://localhost:9000/");
+    // console.log("Server has started at http://localhost:9000");
   } catch (error) {
     console.log("Error is", error);
   }
